2cea65df3728974e1f6f47aa33465163c67ce8f6
nikita <kobalt31@mail.ru>
2018-04-15 02:59:11 +0300

    UserRepository cleanup

--- src/main/java/webform/UserRepository.java
+++ src/main/java/webform/UserRepository.java
@@ -10,28 +10,4 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    /*@Query(value = "SELECT COUNT(id) FROM users WHERE login = :login", nativeQuery = true)
-    int countAllUsersByLogin(
-            @Param("login") String login
-    );
-
-    @Query(value = "SELECT * FROM users WHERE login = :login AND password = :password", nativeQuery = true)
-    User getUserByLoginAndPassword(
-            @Param("login") String login,
-            @Param("password") String password
-    );
-
-    @Transactional
-    @Modifying
-    @Query(value = "INSERT INTO users(login, password) VALUES(:login, :password)", nativeQuery = true)
-    void addUser(
-            @Param("login") String login,
-            @Param("password") String password
-    );
-
-    @Query(value = "SELECT * FROM users WHERE id = :id", nativeQuery = true)
-    User getUserById(
-            @Param("id") Long id
-    );*/
-
 }
